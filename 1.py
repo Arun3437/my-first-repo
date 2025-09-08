2ee1fbcc1f510addb131517fbcada2cc737a875a
Arun3437 <arunk58936@gmail.com>
2025-09-08 11:28:32 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
-n=int(input("Enter the value"));
-v=int(input("Enter the value"));
-print(n+v)
+n=int(input("Enter the value"))
+v=int(input("Enter the value"))
+z=int(input("Enter the value"))
+print(n+v+z)
